63cfc44f242aa6a52ce918f74e5300e170cfaa14
MoritzAha <moritzalthaus@gmx.de>
2019-06-29 17:32:09 +0200

    utils.py added for loss function and other utilities - keeping code nice and tidy

---
+++ src/utils.py
@@ -0,0 +1,16 @@
+import torch
+
+
+# Implement the loss function for the VAE
+def vae_loss(recon_x, x, mu, log_var, loss_func):
+    """
+    :param recon_x: reconstruced input
+    :param x: input
+    :param mu, log_var: parameters of posterior (distribution of z given x)
+    :loss_func: loss function to compare input image and constructed image
+    """
+
+    recon_loss = loss_func(recon_x, x)
+    kl_loss = torch.mean(0.5 * torch.sum(
+        torch.exp(log_var) + mu**2 - 1. - log_var, 1))
+    return recon_loss + kl_loss
