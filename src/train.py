b9c38173f579b195238b81ab3845169b2bf5bbd0
n1ce3 <eliasarnold@outlook.de>
2019-07-04 12:35:47 +0200

    data loader done, save model has to be implemented next

--- src/train.py
+++ src/train.py
@@ -18,7 +18,7 @@ from torchvision import models, transforms
 from utils import vae_loss, set_split
 
 # Training of the VAE
-def train(model, epochs, batch, optimizer, loss_fct, path, subset=None, test_split=0.2):
+def train(model, epochs, batch, optimizer, loss_fct, path, subset_size=None, test_split=0.2):
 
     # set pathes to data
     meta_path = '../data/celebrity2000_meta.mat'
@@ -27,17 +27,17 @@ def train(model, epochs, batch, optimizer, loss_fct, path, subset=None, test_spl
     # not sure if we need normalize, therefore not used in trafo
     PIL = transforms.ToPILImage()
     normalize = transforms.Normalize(mean=0, std=1)
-    grey = transforms.Grayscale(num_output_channels=1)
-    #crop = transforms.CenterCrop(size=64)
+    #grey = transforms.Grayscale(num_output_channels=1)
+    #crop = transforms.CenterCrop(size=32)
     to_tensor = transforms.ToTensor()
 
     # define transformations
-    trafo = transforms.Compose([PIL, grey, to_tensor])
+    trafo = transforms.Compose([PIL, to_tensor])
 
-    # datasets
+    # data sets
     dataset = FaceDataset(meta_path=meta_path, data_dir=data_dir, transform=trafo)
 
-    train_sampler, test_sampler = set_split(dataset, subset=subset, test_split=test_split)
+    train_sampler, test_sampler = set_split(dataset, subset=subset_size, test_split=test_split)
 
     train_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch, sampler=train_sampler)
     test_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch, sampler=test_sampler)
@@ -88,7 +88,7 @@ def train(model, epochs, batch, optimizer, loss_fct, path, subset=None, test_spl
 if __name__ == '__main__':
 
     # needed for macOS in some cases
-    os.environ['KMP_DUPLICATE_LIB_OK']='True'
+    os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
     # allow for cuda
     if torch.cuda.is_available():
@@ -97,13 +97,21 @@ if __name__ == '__main__':
         device = 'cpu'
 
     # hyperparameters
-    batch = 10
-    epochs = 1
+    batch = 100
+    epochs = 5
     latent_dim = 50
+    ### 64x64 ###
     # here zero padding is needed
-    encoder_params = [(1, 32, 4, 2, 1), (32, 64, 4, 2, 1), (64, 128, 4, 2, 1), (128, 256, 4, 2, 1), 256*4*4]
+    encoder_params = [(3, 32, 4, 2, 1), (32, 64, 4, 2, 1), (64, 128, 4, 2, 1), (128, 256, 4, 2, 1), 256*4*4]
     # here zero padding is needed because kernel of seize three needs padding to retain shape after upsampling
-    decoder_params = [256*4*4, (256, 128, 3, 1, 1), (128, 64, 3, 1, 1), (64, 32, 3, 1, 1), (32, 1, 3, 1, 1)]
+    decoder_params = [256*4*4, (256, 128, 3, 1, 1), (128, 64, 3, 1, 1), (64, 32, 3, 1, 1), (32, 3, 3, 1, 1)]
+
+    ### 32x32 ###
+    # here zero padding is needed
+    #encoder_params = [(3, 32, 5, 2, 2), (32, 64, 5, 2, 2), (64, 128, 5, 2, 2), 128*5*5]
+    # here zero padding is needed because kernel of seize three needs padding to retain shape after upsampling
+    #decoder_params = [128*5*5, (128, 64, 5, 2, 2, 1), (64, 32, 5, 2, 2, 1), (32, 3, 5, 2, 2, 1)]
+
     lr = 1e-4
 
     # set up Model
